1a5b6ffae9662040479f9ddf553f9575f8db5c5d
Justin McAllister <justinm@justinm.com>
2019-10-17 18:04:58 -0700

    Fixes #75. Thanks @fdarvis for tracking this down.

--- micasense/imageutils.py
+++ micasense/imageutils.py
@@ -157,8 +157,11 @@ def align(pair):
                 plotutils.plotwithcolorbar(grad2, "match grad level {}".format(level))
                 print("Starting warp for level {} is:\n {}".format(level,warp_matrix))
 
-            cc, warp_matrix = cv2.findTransformECC(grad1, grad2, warp_matrix, warp_mode, criteria, inputMask=None, gaussFiltSize=1)
-
+            try:
+                cc, warp_matrix = cv2.findTransformECC(grad1, grad2, warp_matrix, warp_mode, criteria, inputMask=None, gaussFiltSize=1)
+            except TypeError:
+                cc, warp_matrix = cv2.findTransformECC(grad1, grad2, warp_matrix, warp_mode, criteria)
+            
             if show_debug_images:
                 print("Warp after alignment level {} is \n{}".format(level,warp_matrix))
 
@@ -401,11 +404,14 @@ def min_max(pts):
 def map_points(pts, image_size, warpMatrix, distortion_coeffs, camera_matrix,warp_mode=cv2.MOTION_HOMOGRAPHY):
     # extra dimension makes opencv happy
     pts = np.array([pts], dtype=np.float)
-
     new_cam_mat, _ = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coeffs, image_size, 1)
     new_pts = cv2.undistortPoints(pts, camera_matrix, distortion_coeffs, P=new_cam_mat)
     if warp_mode == cv2.MOTION_AFFINE:
         new_pts = cv2.transform(new_pts, cv2.invertAffineTransform(warpMatrix))
     if warp_mode == cv2.MOTION_HOMOGRAPHY:
         new_pts =cv2.perspectiveTransform(new_pts,np.linalg.inv(warpMatrix).astype(np.float32))
-    return new_pts[0]
+    #apparently the output order has changed in 4.1.1 (possibly earlier from 3.4.3)
+    if cv2.__version__<='3.4.4':
+        return new_pts[0]
+    else:
+        return new_pts[:,0,:]
